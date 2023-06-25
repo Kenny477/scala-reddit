d1a08f67ae9468437ba401269bf6a70cd38f3d5f
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2023-06-24 21:47:50 -0400

    Progress on XML parsing Need to work on better OOP structure Then on actual web display

--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ lazy val root = project
     version := "0.1.0-SNAPSHOT",
     scalaVersion := scala3Version,
     libraryDependencies ++= Seq(
+      "org.scala-lang.modules" %% "scala-xml" % "2.1.0",
       "org.scalameta" %% "munit" % "0.7.29" % Test,
       "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
       "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,18 +6,22 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 
 import scala.concurrent.Future
-import scala.util.{ Failure, Success }
+import scala.util.{Failure, Success}
 import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
 
-object HttpClientSingleRequest {
+import scala.xml._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import scala.concurrent.Await
 
+object RSS {
   val headers: List[HttpHeader] = List()
   def parseHeader(header: String, value: String): List[HttpHeader] = {
-    HttpHeader.parse(header, value) match
+    HttpHeader.parse(header, value) match {
       case Ok(header, errors) => headers :+ header
       case akka.http.scaladsl.model.HttpHeader.ParsingResult.Error(error) =>
         println(error)
         sys.error("something wrong")
+    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -25,20 +29,63 @@ object HttpClientSingleRequest {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.executionContext
 
-    val headers: List[HttpHeader] = List()
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(
+      HttpRequest(uri = "https://www.reddit.com/r/scala.rss")
+    )
 
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "https://www.reddit.com/r/scala.rss"))
+    val response = Await.result(
+      responseFuture.flatMap(resp => Unmarshal(resp.entity).to[String]),
+      scala.concurrent.duration.Duration.Inf
+    )
 
-    responseFuture
-      .onComplete {
-        case Success(res) => 
-          println(res)
-          // Get data from response
-          res.entity.dataBytes.runForeach { chunk =>
-            println(chunk.utf8String)
-          }
+    // println(response)
 
-        case Failure(_)   => sys.error("something wrong")
-      }
+    val xml = XML.loadString(response)
+
+    val entries = xml \\ "entry"
+
+    /* 
+      Each entry follows the following format
+      <entry>
+        <author>
+          <name>...</name>
+          <uri>...</uri>
+        </author>
+        <category />
+        <content>...</content>
+        <id>...</id>
+        <link href="..." />
+        <updated>...</updated>
+        <published>...</published>
+        <title>...</title>
+      </entry>
+    */
+
+    case class Entry(
+      authorName: String,
+      authorUri: String,
+      category: String,
+      content: String,
+      id: String,
+      link: String,
+      updated: String,
+      published: String,
+      title: String
+    )
+
+
+    val data = entries.map { entry =>
+      Entry(
+        (entry \ "author" \ "name").text,
+        (entry \ "author" \ "uri").text,
+        (entry \ "category").text,
+        (entry \ "content").text,
+        (entry \ "id").text,
+        (entry \ "link" \ "@href").text,
+        (entry \ "updated").text,
+        (entry \ "published").text,
+        (entry \ "title").text
+      )
+    }
   }
 }
